81bd2d8f1fe89f8257080bfa8741002ec4c3544c
Luiz Batanero <luizbatanero@gmail.com>
2019-07-01 13:55:03 -0300

    Import image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './App.css';
+import profile from './assets/profile.png';
 
 function App() {
-  return <h1>App</h1>;
+  return <img src={profile} width="200" />;
 }
 
 export default App;
